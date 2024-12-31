e59712a8e08be8629fb9bf03c6e97dd34492cfa3
Thamaraikannan-creator <thamaraik915@gmail.com>
2024-12-31 12:31:38 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <p>&copy; {new Date().getFullYear()} Thamaraikannan. All rights reserved.</p>
        
         <div className="footer-social">
-          <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Thamaraikannan-creator" target="_blank" rel="noopener noreferrer">
             <img src="https://img.icons8.com/material-outlined/24/000000/github.png" alt="GitHub" />
           </a>
           <a href="https://www.linkedin.com/in/thamaraikannan2003/" target="_blank" rel="noopener noreferrer">
